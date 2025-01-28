beb2176ce90c9f8a82943b70ba1e930d7e69b975
JIbald <j.ibald@protonmail.ch>
2025-01-28 19:02:59 +0100

    remove unnecessary line

--- init.lua
+++ init.lua
@@ -155,8 +155,6 @@ vim.opt.rtp:prepend(lazypath)
 --  To check the current status of your plugins, run
 --    :Lazy
 --
---  You can press `?` in this menu for help. Use `:q` to close the window
---
 --  To update plugins you can run
 --    :Lazy update
 --
